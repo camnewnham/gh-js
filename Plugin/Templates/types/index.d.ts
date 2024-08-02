847bde6302ce5c58f56a23cb15c082f5bbd6a7c2
camnewnham <camnewnham@gmail.com>
2024-08-02 13:17:58 +1000

    declare global type of dotnet object

--- Plugin/Templates/types/index.d.ts
+++ Plugin/Templates/types/index.d.ts
@@ -1,6 +1,10 @@
-export { RhinoCommon } from './RhinoCommon';
-export { Grasshopper } from './Grasshopper';
-export { Rhino.UI } from './Rhino.UI';
-export { GH_IO } from './GH_IO';
-export { GH_Util } from './GH_Util';
-export { Eto } from './Eto';
+import "./RhinoCommon";
+import "./Grasshopper";
+import "./Rhino.UI";
+import "./GH_IO";
+import "./GH_Util";
+import "./Eto";
+
+declare global {
+    const dotnet: typeof import("node-api-dotnet");
+}
