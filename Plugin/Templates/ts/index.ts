e4329c50a5e747335d75d21b30c1774b7b9a7455
cam <camnewnham@gmail.com>
2024-08-09 12:33:20 +1000

    support type checking in JS

--- Plugin/Templates/ts/index.ts
+++ Plugin/Templates/ts/index.ts
@@ -1,12 +1,13 @@
-﻿/**
+﻿/// <reference types="./types/component.d.ts" />
+/**
  * Executes within the context of the Grasshopper component.
- * @param inputs An object with keys matching the inputs to the Grasshopper component
- * @param context The context in which the Grasshopper component is executed
- * @returns An object with keys matching the outputs from the Grasshopper component
+ * @param {Inputs} inputs An object with keys matching the inputs to the Grasshopper component
+ * @param {Context} context The context in which the Grasshopper component is executed
+ * @returns {Outputs} An object with keys matching the outputs from the Grasshopper component
  */
 
 export async function runScript(inputs: Inputs, context: Context): Promise<Outputs> {
-    const sum: number = (inputs.a ?? 0) + (inputs.b ?? 0);
+  const sum: number = (inputs.a ?? 0) + (inputs.b ?? 0);
   return {
       x: sum
   };
